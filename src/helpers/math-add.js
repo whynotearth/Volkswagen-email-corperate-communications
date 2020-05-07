45dfa5b6cc8060db1725338499e42e994fb880d9
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-05-07 10:41:00 +0430

    add math helpers, settings, is-number package, minor styles in scss

---
+++ src/helpers/math-add.js
@@ -0,0 +1,11 @@
+var isNumber = require('is-number');
+
+module.exports = function (a, b) {
+  if (isNumber(a) && isNumber(b)) {
+    return Number(a) + Number(b);
+  }
+  if (typeof a === "string" && typeof b === "string") {
+    return a + b;
+  }
+  return "";
+};
