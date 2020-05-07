d82e9bd24694e6939a57949b4445c8ecb4b96e03
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-05-07 21:13:47 +0430

    update readme, remove unused helpers

--- test-backend/handlebars-compile.js
+++ test-backend/handlebars-compile.js
@@ -1,8 +1,6 @@
 const Handlebars = require("handlebars");
 const rootPath = '../'
 Handlebars.registerHelper('cloudinary_transform', require(rootPath + 'src/helpers/cloudinary_transform.js'));
-Handlebars.registerHelper('equals', require(rootPath + 'src/helpers/equals.js'));
-Handlebars.registerHelper('not_equals', require(rootPath + 'src/helpers/not_equals.js'));
 var fs = require('fs');
 const htmlFilePath = rootPath + 'dist/two-column.html';
 
