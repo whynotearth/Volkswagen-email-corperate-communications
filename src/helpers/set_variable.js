de5950677e6dd296881e9397912049db9e457b15
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-05-10 15:19:19 +0430

    add triple articles, resize label of article light from 150 to 128

--- src/helpers/set_variable.js
+++ src/helpers/set_variable.js
@@ -1,6 +1,6 @@
 // create a root variable
 // example:
-// {{set_variable "myVariable" (math-minus articleWidth @root.settings.articleLightStaticPartsWidth)}}
+// {{set_variable "myVariable" (math-minus articleWidth @root.settings.someVariable)}}
 // usage:
 // {{@root.myVariable}}
 module.exports = function (variableName, variableValue, options) {
